5248b363469b4c881b41dc6dc50625f8f2313feb
Javier Gonzalez <gonzakick@gmail.com>
2019-03-13 21:31:42 +0100

    Arreglado fallo conexion

--- ShareMusicBack/src/database/queries.js
+++ ShareMusicBack/src/database/queries.js
@@ -1,5 +1,36 @@
 // Base de datos
-const {conection } =  require('./database');
+var mysql = require('mysql');
+//datos de configuración BD en Heroku
+var conection_cfg = {
+    host:'eu-cdbr-west-02.cleardb.net' ,
+    user:'b5a7ddd37c8593',
+    password:'64f574cf',
+    database:'heroku_efb95a0eab20b17'
+
+};
+
+var conection;
+// mySql cierra la conexion cuando se le agota el timeout, 
+// volver a lanzar
+conection = mysql.createConnection(conection_cfg);
+function connectionHandler(){
+    conection = mysql.createConnection(conection_cfg);
+    conection.connect(function (err) {
+        if(err){
+            console.log('Error al conectar a bd');
+        }
+        console.log('Conectado con bd')
+    });
+    conection.on('error', function(err) {
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.log('Reconectando a bd...');
+            connectionHandler();
+        } else {
+            throw err;
+        }
+    });
+}
+connectionHandler();
 
 const usuario_login = function (data,res) {
     let sql = 'SELECT Nombre, NombreReal, Email FROM usuario WHERE Email = ?  AND Contraseña = ?'
