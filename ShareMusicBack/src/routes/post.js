7bb914a94afe783f1e2856da71565e1f55cc6506
javiermixture17 <javiermf.98@gmail.com>
2019-04-19 20:11:44 +0200

    Operaciones a realizar

--- ShareMusicBack/src/routes/post.js
+++ ShareMusicBack/src/routes/post.js
@@ -10,3 +10,33 @@ router.post('/editarPost',posts.add_post);
 
 
 module.exports=router;*/
+
+
+
+
+//Publicar post
+    //Parámetros: nombre usuario, título(si lo añadirmos a la BBDD), cuerpo
+
+//Obtener posts de un usuario concreto:
+    //Parámetros: nombre usuario
+    //Devolución: 
+        //Lista de posts del usuario concreto: 
+            //Cada post: id del post, usuario creador del post y contenido del post (título y cuerpo)
+
+//Obtener posts del timeline de un usuario:
+    //Parámetros: nombre usuario
+    //Devolución: 
+        //Lista de posts de los usuarios a los que sigue el usuario:
+            //Cada post: id del post, usuario creador del post y contenido del post (título y cuerpo)
+
+//Editar post
+    //Parámetros: id del post, título(si lo añadirmos a la BBDD), cuerpo
+
+//Eliminar post
+    //Parámetros: id del post
+
+//Me gusta post
+    //Parámetros: id del post
+
+//No me gusta
+    //Parámetros: id del post
